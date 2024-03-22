a89ba111b29e012fe4a1bf77726cafb2b6faa362
mlin865 <mlin865@aucklanduni.ac.nz>
2024-03-22 15:20:20 +1300

    Add plugin files for data downsampling

---
+++ README.rst
@@ -0,0 +1,5 @@
+Data Downsampler
+================
+
+The Data Downsampler step is a plugin for the MAP Client application.
+
---
+++ mapclientplugins/datadownsamplerstep/__init__.py
@@ -0,0 +1,16 @@
+
+"""
+MAP Client Plugin - Generated from MAP Client v0.20.0
+"""
+
+__version__ = '0.1.0'
+__author__ = 'Mabelle Lin'
+__stepname__ = 'Data Downsampler'
+__location__ = ''
+
+# import class that derives itself from the step mountpoint.
+from mapclientplugins.datadownsamplerstep import step
+
+# Import the resource file when the module is loaded,
+# this enables the framework to use the step icon.
+from . import resources_rc
